2c2f7ce6419bb49733da25ab0ed2d5ffae8ad071
MarkHaihel <markiyanse@gmail.com>
2021-12-05 17:06:42 +0200

    Add string Includes to Base Specifications

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -20,6 +20,7 @@ namespace Infrastructure.Data
             }
 
             query = spec.Includes.Aggregate(query, (curent, include) => curent.Include(include));
+            query = spec.IncludeStrings.Aggregate(query, (current, include) => current.Include(include));
 
             return query;
         }
