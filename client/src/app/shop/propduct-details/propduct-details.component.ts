d331f45f90fcc960dfc2b60bb64e4caf8986d487
MarkHaihel <markiyanse@gmail.com>
2022-01-29 14:57:18 +0200

    End of section 10

---
+++ client/src/app/shop/propduct-details/propduct-details.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
+
+@Component({
+  selector: 'app-propduct-details',
+  templateUrl: './propduct-details.component.html',
+  styleUrls: ['./propduct-details.component.scss']
+})
+export class PropductDetailsComponent implements OnInit {
+  product: IProduct
+
+  constructor(private shopService: ShopService, private activateRoot: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    this.shopService.getProduct(+this.activateRoot.snapshot.paramMap.get('id')).subscribe(product => {
+      this.product = product;
+    }, error => {
+      console.log(error);
+    })
+  }
+
+}
