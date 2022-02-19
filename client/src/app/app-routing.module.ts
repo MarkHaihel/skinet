bb21c5f313ad7e784406cb1b7269272174cb0d3b
MarkHaihel <markiyanse@gmail.com>
2022-02-19 02:17:01 +0200

    fix infinity load for account module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -43,7 +43,6 @@ const routes: Routes = [
   },
   {
     path: 'account',
-    canActivate: [AuthGuard],
     loadChildren: () => import('./account/account.module')
       .then(mod => mod.AccountModule),
     data: { breadcrumb: { skip: true } }
